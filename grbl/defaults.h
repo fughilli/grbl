07b68679504d0eb48ef1ba486cb54f8ea5b68670
Kevin Balke <kbalke@ucla.edu>
2020-05-01 22:17:30 -0700

    Updated defaults for Trinamic upgrade

--- grbl/defaults.h
+++ grbl/defaults.h
@@ -535,9 +535,9 @@
   #define DEFAULT_X_STEPS_PER_MM 200
   #define DEFAULT_Y_STEPS_PER_MM 200
   #define DEFAULT_Z_STEPS_PER_MM 200
-  #define DEFAULT_X_MAX_RATE 3000.0 // mm/min
-  #define DEFAULT_Y_MAX_RATE 3000.0 // mm/min
-  #define DEFAULT_Z_MAX_RATE 3000.0 // mm/min
+  #define DEFAULT_X_MAX_RATE 1300.0 // mm/min
+  #define DEFAULT_Y_MAX_RATE 1300.0 // mm/min
+  #define DEFAULT_Z_MAX_RATE 1300.0 // mm/min
   #define DEFAULT_X_ACCELERATION (40.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
   #define DEFAULT_Y_ACCELERATION (40.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
   #define DEFAULT_Z_ACCELERATION (40.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
@@ -548,8 +548,8 @@
   #define DEFAULT_SPINDLE_RPM_MIN 0.0 // rpm
   #define DEFAULT_STEP_PULSE_MICROSECONDS 10
   #define DEFAULT_STEPPING_INVERT_MASK 0
-  #define DEFAULT_DIRECTION_INVERT_MASK 0b1010
-  #define DEFAULT_STEPPER_IDLE_LOCK_TIME 25 // msec (0-254, 255 keeps steppers enabled)
+  #define DEFAULT_DIRECTION_INVERT_MASK 0b0101
+  #define DEFAULT_STEPPER_IDLE_LOCK_TIME 255 // msec (0-254, 255 keeps steppers enabled)
   #define DEFAULT_STATUS_REPORT_MASK 1 // MPos enabled
   #define DEFAULT_JUNCTION_DEVIATION 0.02 // mm
   #define DEFAULT_ARC_TOLERANCE 0.002 // mm
@@ -563,9 +563,9 @@
   #define DEFAULT_HOMING_ENABLE 1  // false
   #define DEFAULT_HOMING_DIR_MASK 0b001 // move positive dir
   #define DEFAULT_HOMING_FEED_RATE 25.0 // mm/min
-  #define DEFAULT_HOMING_SEEK_RATE 2000.0 // mm/min
+  #define DEFAULT_HOMING_SEEK_RATE 1300.0 // mm/min
   #define DEFAULT_HOMING_DEBOUNCE_DELAY 250 // msec (0-65k)
-  #define DEFAULT_HOMING_PULLOFF 1.0 // mm
+  #define DEFAULT_HOMING_PULLOFF 1.5 // mm
 #endif
 
 #ifdef DEFAULTS_SIMULATOR
